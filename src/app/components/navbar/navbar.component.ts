f84abae641abb2d30e7195e49ed101205da758d0
VicVaporu01 <viccjuan@gmail.com>
2024-01-01 19:08:07 -0500

    style(navbar): Navbar Style
    Navbar style fixed

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import {HomeComponent} from "../home/home.component";
+import {NgOptimizedImage} from "@angular/common";
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-    imports: [
-        HomeComponent
-    ],
+  imports: [
+    HomeComponent,
+    NgOptimizedImage
+  ],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
